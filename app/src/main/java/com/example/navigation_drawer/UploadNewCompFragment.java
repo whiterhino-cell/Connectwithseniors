e1c8fbb7d54f6bfa0aa959cb3e9a0cc22eee8bfd
Anshu Kumari <anshuank777@gmail.com>
2022-07-30 21:08:05 +0530

    Upload Fragment added and integrated to firebase

---
+++ app/src/main/java/com/example/navigation_drawer/UploadNewCompFragment.java
@@ -0,0 +1,22 @@
+package com.example.navigation_drawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UploadNewCompFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_upload_new_comp, container, false);
+
+        return  view;
+    }
+}

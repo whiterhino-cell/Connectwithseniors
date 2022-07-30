3a4c3e2a7e24a0db40e36d9094254720253aec8f
Anshu Kumari <anshuank777@gmail.com>
2022-07-30 21:36:43 +0530

    UploadNewCompFragment Updated

--- app/src/main/java/com/example/navigation_drawer/UploadNewCompFragment.java
+++ app/src/main/java/com/example/navigation_drawer/UploadNewCompFragment.java
@@ -4,19 +4,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 public class UploadNewCompFragment extends Fragment {
+    private static final String TAG = "UpNewCoFragment started";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_upload_new_comp, container, false);
+        View view = inflater.inflate(R.layout.fragment_upload_new_comp, container, false);
+        String campus = getArguments().getString("campus");
+        Log.d(TAG, "onCreateView: "+campus);
 
-        return  view;
+        TextView textView=view.findViewById(R.id.new_comp_upTV);
+        textView.setText(campus);
+
+        return view;
     }
 }
